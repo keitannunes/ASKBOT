d86d8bd4249a8241ca12a91fa845113b791865b2
keitannunes <keitannunes@gmail.com>
2021-04-21 12:21:18 -0400

    feat: deleted sell

--- index.js
+++ index.js
@@ -32,11 +32,6 @@ async function getID(usrnm) { //function to get account ID from username
   return response.data.data[0].account_id
 }
 
-const questions = [
-  'What do you want to sell?',
-  'What amount?',
-  'What price would you like to list it at?'
-]
 client.on("message", async message => {
 
   const messageLower = message.content.toLowerCase();
@@ -56,40 +51,6 @@ client.on("message", async message => {
       output.edit(
         `Pong! Latency is ${output.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`
       );
-      break;
-
-
-
-
-    case "sell":
-      const filter = (m) => {
-        return m.author.id === message.author.id
-      };
-      let counter = 0
-      let replyArray = ["Answer1", "Answer2", "Answer3"];
-      const collector = message.channel.createMessageCollector(filter, { max: questions.length, time: 30000 });
-      output.edit(questions[counter++]);
-
-      collector.on('collect', m => {
-        replyArray[counter-1] = m.content;
-        if (counter < questions.length) {
-          m.channel.send(questions[counter++])
-        }
-      });
-
-      collector.on('end', collected => {
-        console.log(`Collected ${collected.size} items`);
-        let counter = 0
-        collected.forEach((value) => {
-          console.log(questions[counter++], value.content)
-        })
-        if (collected.size < questions.length) {
-          message.reply("You did not answer the questions in time (30 sec)")
-          return
-        } else {
-          message.channel.send(`X${replyArray[1]} ${replyArray[0]} - $${replyArray[2]}`)
-        }
-      });
       break;
     case "changegame":
       const text = message.content.substr(",changegame ".length);

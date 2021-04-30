3387314691fa10113fe6b1eb2d245821badbc47e
keitannunes <keitannunes@gmail.com>
2021-04-30 14:17:19 -0400

    feat: introduced aww command

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const http = require("http");
 const fs = require("fs");
 const discord = require("discord.js");
 const client = new discord.Client();
-const axios = require("axios")
+const path = require('path');
+const axios = require('axios');
+const redditApiImageGetter = require('reddit-api-image-getter')
+const getter = new redditApiImageGetter()
+
 require('dotenv').config();
 
 client.channels.cache.get()
@@ -27,7 +31,7 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-
+ 
   const messageLower = message.content.toLowerCase();
   const args = message.content
     .slice(process.env.PREFIX.length)
@@ -35,17 +39,16 @@ client.on("message", async message => {
     .split(/ +/g);
 
   const command = args.shift().toLowerCase();
-  if (message.author.bot) return;
-  if (message.content.indexOf(process.env.PREFIX) !== 0) return;
+  if (message.author.bot) return; //ignores messages from bots
+  if (!message.member.roles.cache.has('837731710468751400')) return; //ignores messages from uesrs without role
+  if (message.content.indexOf(process.env.PREFIX) !== 0) return; //ignores messages without prefix in .env file
   const output = await message.channel.send("Thinking..."); //Response that we are gonna edit
+  
   //COMMANDS!!!!!!
   switch (command) {
     case "ping":
       console.log(`${message.author.username} used ping`);
-      //output.edit(
-      //  `Pong! Latency is ${output.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`
-      //);
-      output.edit('Stop')
+      output.edit(`Pong! Latency is ${output.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
       break;
     case "ask":
       output.edit('Are you ok?')
@@ -55,7 +58,7 @@ client.on("message", async message => {
       };
 
       const collector = message.channel.createMessageCollector(filter, { time: 15000, max: 1 }); // declare collector with max wait time 15 sec and 1 reply
-      
+
       collector.on('collect', m => {//runs when collected something
         console.log(`Collected ${m.content}`);
         userReplied = true;
@@ -68,7 +71,24 @@ client.on("message", async message => {
         }
       });
       break;
-    default:
+
+    case "aww":
+      //gets new hot images from r/aww and puts it in /images/hot/aww
+      getter.getHotImagesOfSubReddit('aww').then(function (result) {
+        for (imageEntry of result) {
+          const targetDirectory = path.resolve(__dirname, 'images', 'hot');
+          getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
+        }
+      })
+
+      //chooses random file from dir and replies to user
+      var files = fs.readdirSync(__dirname + '/images/hot/aww/')
+      chosenFile = files[Math.floor(Math.random() * files.length)]
+      output.delete()//delets the thinking... message
+      message.reply("Here is an image from r/aww", { files: ['images/hot/aww/' + chosenFile] })//sends file
+      break;
+
+    default://when command doens't match any cases
       output.edit("That's not a command!")
   }
 });

2d14663becdf3c79d3021b7b15465eabbd8468bf
keitannunes <keitannunes@gmail.com>
2021-05-02 19:22:20 -0400

    feat: integrated LUIS into askbot command

--- index.js
+++ index.js
@@ -10,10 +10,44 @@ const discord = require("discord.js");
 const client = new discord.Client();
 const path = require('path');
 const redditApiImageGetter = require('reddit-api-image-getter')
-const getter = new redditApiImageGetter()
-const axios = require("axios")
-
+const getter = new redditApiImageGetter();
+const axios = require("axios");
+const { data } = require("jquery");
 require('dotenv').config();
+
+awwDownloaded = false;
+memeDownloaded = true;
+
+//Functions
+async function luisPrediction(query) {
+  const response = await axios.get(process.env.AZURE_URL + query)
+  return response.data.prediction;
+}
+
+function sendImage(response) {
+  if (response.entities.imageType[0].hasOwnProperty('aww')) {
+    //gets new hot images from r/aww and puts it in /images/hot/aww
+    getter.getHotImagesOfSubReddit('aww').then(function (result) {
+      for (imageEntry of result) {
+        const targetDirectory = path.resolve(__dirname, 'images', 'hot');
+        getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
+      }
+    })
+    //r/eyebleach
+    getter.getHotImagesOfSubReddit('eyebleach').then(function (result) {
+      for (imageEntry of result) {
+        const targetDirectory = path.resolve(__dirname, 'images', 'hot');
+        getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
+      }
+    })
+    //chooses random file from dir and replies to user
+    var files = fs.readdirSync(__dirname + '/images/hot/aww/')
+    chosenFile = files[Math.floor(Math.random() * files.length)]
+    output.edit("Here is an image from r/aww", { files: ['images/hot/aww/' + chosenFile] })//returns file
+  }
+  else {
+  }
+}
 
 client.channels.cache.get()
 console.clear()
@@ -37,8 +71,6 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
- 
-  const messageLower = message.content.toLowerCase();
   const args = message.content
     .slice(process.env.PREFIX.length)
     .trim()
@@ -48,34 +80,97 @@ client.on("message", async message => {
   if (message.author.bot) return; //ignores messages from bots
   if (!message.member.roles.cache.has('837731710468751400')) return; //ignores messages from uesrs without role
   if (message.content.indexOf(process.env.PREFIX) !== 0) return; // ignores messages without prefix in .env file
-  const output = await message.channel.send("Thinking..."); //Response that we are gonna edit
+  output = await message.channel.send("Thinking..."); //Response that we are gonna edit
 
   //COMMANDS!!!!!!
   switch (command) {
+
     case "ping":
       console.log(`${message.author.username} used ping`);
-      output.edit(`Pong! Latency is ${output.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+      output.edit(`Pong! Latency is ${output.createdTimestamp - message.createdTimestamp}ms.`);
       break;
-    case "ask":
-      output.edit("Hey! It's Askbot! How may I help you?")
-      var userReplied = false
-      const filter = (m) => { //define filter (i actually dont know what this does)
-        return m.author.id === message.author.id
-      };
-
-      const collector = message.channel.createMessageCollector(filter, { time: 15000, max: 1 }); // declare collector with max wait time 15 sec and 1 reply
-
-      collector.on('collect', m => {//runs when collected something
-        console.log(`Collected ${m.content}`);
-        userReplied = true;
-      });
-
-      collector.on('end', collected => { //runs when collector stops collecting
-        console.log(`Collected ${collected.size} items`);
-        if (!userReplied) { //checks if man dont respond
-          message.channel.send("Are you still here?")
+    /*
+        case "ask":
+          output.edit("Hey! It's Askbot! How may I help you?")
+          var userReplied = false
+          const filter = (m) => { //define filter 
+            return m.author.id === message.author.id
+          };
+    
+            const collector = message.channel.createMessageCollector(filter, { time: 30000, max: 1 }); // declare collector with max wait time 15 sec and 1 reply
+    
+            collector.on('collect', m => {//runs when collected something
+              
+              luisPrediction(m.content).then((intent) => {
+                if (intent === "sendImage") {
+                  message.reply("I will send an image")
+                }else{
+                 console.log(intent)
+                }
+                userReplied = true;
+              });
+            });
+    
+            collector.on('end', collected => { //runs when collector stops collecting
+              console.log(`Collected ${collected.size} items`);
+              if (!userReplied) { //checks if man dont respond
+                message.channel.send("Are you still here?")
+              }
+            });
+          break;
+    */
+    case "askbot":
+      luisResponse = await luisPrediction(message.content.slice(8))
+      console.log("Top Intent: "+luisResponse.topIntent);
+      console.log("Entities:" + luisResponse.entities)
+      if (luisResponse.topIntent === "sendImage") {
+        //Animal Image
+        if (luisResponse.entities.imageType[0].hasOwnProperty('aww')) {
+          
+          if (!awwDownloaded){
+          //gets new hot images from r/aww and puts it in /images/hot/aww
+          getter.getHotImagesOfSubReddit('aww').then(function (result) {
+            for (imageEntry of result) {
+              const targetDirectory = path.resolve(__dirname, 'images', 'hot');
+              getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
+            }
+          })
+          //r/eyebleach
+          getter.getHotImagesOfSubReddit('eyebleach').then(function (result) {
+            for (imageEntry of result) {
+              const targetDirectory = path.resolve(__dirname, 'images', 'hot');
+              getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
+            }
+          })
+          awwDownloaded = true;
         }
-      });
+          //chooses random file from dir and replies to user
+          var files = fs.readdirSync(__dirname + '/images/hot/aww/')
+          chosenFile = files[Math.floor(Math.random() * files.length)]
+          message.reply("Here is an image from r/aww", { files: ['images/hot/aww/' + chosenFile] })//returns file
+          output.delete() //delete thinking.. messag
+        }
+        else if (luisResponse.entities.imageType[0].hasOwnProperty('meme')) {
+          if (!memeDownloaded){
+          //gets new hot images from r/wholesomememes and puts it in /images/hot/wholesomememes
+          getter.getHotImagesOfSubReddit('wholesomememes').then(function (result) {
+            for (imageEntry of result) {
+              const targetDirectory = path.resolve(__dirname, 'images', 'hot');
+              getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
+            }
+          });
+          memeDownloaded = true;
+        }
+          //chooses random file from dir and replies to user
+          var files = fs.readdirSync(__dirname + '/images/hot/wholesomememes/')
+          chosenFile = files[Math.floor(Math.random() * files.length)]
+          message.reply("Here is an image from r/wholesomememes", { files: ['images/hot/wholesomememes/' + chosenFile] })//sends file
+          output.delete()//delets the thinking... message
+        }
+      } else {
+        console.log(luisResponse.topIntent)
+      }
+
       break;
 
     case "aww":
@@ -101,8 +196,8 @@ client.on("message", async message => {
       break;
 
     case "wholesomememes":
-       //gets new hot images from r/wholesomememes and puts it in /images/hot/wholesomememes
-       getter.getHotImagesOfSubReddit('wholesomememes').then(function (result) {
+      //gets new hot images from r/wholesomememes and puts it in /images/hot/wholesomememes
+      getter.getHotImagesOfSubReddit('wholesomememes').then(function (result) {
         for (imageEntry of result) {
           const targetDirectory = path.resolve(__dirname, 'images', 'hot');
           getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
@@ -115,21 +210,21 @@ client.on("message", async message => {
       message.reply("Here is an image from r/wholesomememes", { files: ['images/hot/wholesomememes/' + chosenFile] })//sends file
       output.delete()//delets the thinking... message
       break;
+
     case "quote":
       axios.get('https://zenquotes.io/api/random').then(function (response) {
-      output.edit(response.data[0].q)
+        output.edit(response.data[0].q)
       })
-      .catch(function (error) {
-        console.log(error);
-        output.edit(error)
-      })
+        .catch(function (error) {
+          console.log(error);
+          output.edit(error)
+        })
       break;
     case "affirmation":
       axios.get('https://www.affirmations.dev').then(function (response) {
         output.edit(response.data.affirmation)
       }).catch(function (error) {
         console.log(error);
-        output.edit(error);
       })
       break;
     default://when command doens't match any cases

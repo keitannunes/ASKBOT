dd5e7d3feb70615c5d79ed685fc49a41946e5b48
keitannunes <keitannunes@gmail.com>
2021-05-03 18:50:21 -0400

    feat: introduced None response

--- index.js
+++ index.js
@@ -12,43 +12,32 @@ const path = require('path');
 const redditApiImageGetter = require('reddit-api-image-getter')
 const getter = new redditApiImageGetter();
 const axios = require("axios");
-const { data } = require("jquery");
+const { isEmptyObject } = require("jquery");
 require('dotenv').config();
 
+//stuff
 awwDownloaded = false;
 memeDownloaded = true;
-
+const noneResponse = [
+  "Sorry, I didn't get that",
+  "Sorry, can you repeat that?",
+  "Sorry, I didn't quite understand. Can you repeat that?",
+  "Sorry, I didn't get that. Hint: ask me what I can do to get our full feature list!",
+  "Sorry, I didn't get that. If you feel like something is wrong, contact my developer at keifun#9588"
+]
 //Functions
 async function luisPrediction(query) {
   const response = await axios.get(process.env.AZURE_URL + query)
   return response.data.prediction;
 }
 
-function sendImage(response) {
-  if (response.entities.imageType[0].hasOwnProperty('aww')) {
-    //gets new hot images from r/aww and puts it in /images/hot/aww
-    getter.getHotImagesOfSubReddit('aww').then(function (result) {
-      for (imageEntry of result) {
-        const targetDirectory = path.resolve(__dirname, 'images', 'hot');
-        getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
-      }
-    })
-    //r/eyebleach
-    getter.getHotImagesOfSubReddit('eyebleach').then(function (result) {
-      for (imageEntry of result) {
-        const targetDirectory = path.resolve(__dirname, 'images', 'hot');
-        getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
-      }
-    })
-    //chooses random file from dir and replies to user
-    var files = fs.readdirSync(__dirname + '/images/hot/aww/')
-    chosenFile = files[Math.floor(Math.random() * files.length)]
-    output.edit("Here is an image from r/aww", { files: ['images/hot/aww/' + chosenFile] })//returns file
+function isEmpty(obj) {
+  for(var key in obj) {
+      if(obj.hasOwnProperty(key))
+          return false;
   }
-  else {
-  }
+  return true;
 }
-
 client.channels.cache.get()
 console.clear()
 console.log("Loading... Please wait");
@@ -59,9 +48,12 @@ const server = http.createServer((req, res) => {
 });
 server.listen(3000);
 
+//Timeout error
 const error1 = setTimeout(function () {
   console.log("\x1b[41m", "ERROR: UNABLE TO CONNECT TO DISCORD SERVER");
 }, 10000);
+
+
 client.on("ready", () => {
   clearTimeout(error1);
   console.log("");
@@ -70,6 +62,7 @@ client.on("ready", () => {
   client.user.setActivity(fs.readFileSync("views/game.txt", "utf8"));
 });
 
+//runs when bot gets message
 client.on("message", async message => {
   const args = message.content
     .slice(process.env.PREFIX.length)
@@ -82,7 +75,7 @@ client.on("message", async message => {
   if (message.content.indexOf(process.env.PREFIX) !== 0) return; // ignores messages without prefix in .env file
   output = await message.channel.send("Thinking..."); //Response that we are gonna edit
 
-  //COMMANDS!!!!!!
+  //Commands
   switch (command) {
 
     case "ping":
@@ -121,58 +114,69 @@ client.on("message", async message => {
     */
     case "askbot":
       luisResponse = await luisPrediction(message.content.slice(8))
-      console.log("Top Intent: "+luisResponse.topIntent);
+      console.log("Top Intent: " + luisResponse.topIntent);
       console.log("Entities:" + luisResponse.entities)
-      if (luisResponse.topIntent === "sendImage") {
-        //Animal Image
-        if (luisResponse.entities.imageType[0].hasOwnProperty('aww')) {
-          
-          if (!awwDownloaded){
-          //gets new hot images from r/aww and puts it in /images/hot/aww
-          getter.getHotImagesOfSubReddit('aww').then(function (result) {
-            for (imageEntry of result) {
-              const targetDirectory = path.resolve(__dirname, 'images', 'hot');
-              getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
-            }
-          })
-          //r/eyebleach
-          getter.getHotImagesOfSubReddit('eyebleach').then(function (result) {
-            for (imageEntry of result) {
-              const targetDirectory = path.resolve(__dirname, 'images', 'hot');
-              getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
-            }
-          })
-          awwDownloaded = true;
-        }
-          //chooses random file from dir and replies to user
-          var files = fs.readdirSync(__dirname + '/images/hot/aww/')
-          chosenFile = files[Math.floor(Math.random() * files.length)]
-          message.reply("Here is an image from r/aww", { files: ['images/hot/aww/' + chosenFile] })//returns file
-          output.delete() //delete thinking.. messag
-        }
-        else if (luisResponse.entities.imageType[0].hasOwnProperty('meme')) {
-          if (!memeDownloaded){
-          //gets new hot images from r/wholesomememes and puts it in /images/hot/wholesomememes
-          getter.getHotImagesOfSubReddit('wholesomememes').then(function (result) {
-            for (imageEntry of result) {
-              const targetDirectory = path.resolve(__dirname, 'images', 'hot');
-              getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
-            }
-          });
-          memeDownloaded = true;
-        }
-          //chooses random file from dir and replies to user
-          var files = fs.readdirSync(__dirname + '/images/hot/wholesomememes/')
-          chosenFile = files[Math.floor(Math.random() * files.length)]
-          message.reply("Here is an image from r/wholesomememes", { files: ['images/hot/wholesomememes/' + chosenFile] })//sends file
-          output.delete()//delets the thinking... message
-        }
-      } else {
-        console.log(luisResponse.topIntent)
-      }
 
+
+      switch (luisResponse.topIntent) {
+        case "sendImage":
+          //Animal Image
+          if (isEmpty(luisResponse.entities)){//when there are no entities
+            output.edit(noneResponse[Math.floor(Math.random() * noneResponse.length)]);
+            return;
+          } 
+          if (luisResponse.entities.imageType[0].hasOwnProperty('aww')) {
+            if (!awwDownloaded) {
+              //gets new hot images from r/aww and puts it in /images/hot/aww
+              getter.getHotImagesOfSubReddit('aww').then(function (result) {
+                for (imageEntry of result) {
+                  const targetDirectory = path.resolve(__dirname, 'images', 'hot');
+                  getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
+                }
+              })
+              //r/eyebleach
+              getter.getHotImagesOfSubReddit('eyebleach').then(function (result) {
+                for (imageEntry of result) {
+                  const targetDirectory = path.resolve(__dirname, 'images', 'hot');
+                  getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
+                }
+              })
+              awwDownloaded = true;
+            }
+            //chooses random file from dir and replies to user
+            var files = fs.readdirSync(__dirname + '/images/hot/aww/')
+            chosenFile = files[Math.floor(Math.random() * files.length)]
+            message.reply("Here is an image from r/aww", { files: ['images/hot/aww/' + chosenFile] })//returns file
+            output.delete() //delete thinking.. messag
+          }
+          else if (luisResponse.entities.imageType[0].hasOwnProperty('meme')) {
+            if (!memeDownloaded) {
+              //gets new hot images from r/wholesomememes and puts it in /images/hot/wholesomememes
+              getter.getHotImagesOfSubReddit('wholesomememes').then(function (result) {
+                for (imageEntry of result) {
+                  const targetDirectory = path.resolve(__dirname, 'images', 'hot');
+                  getter.saveRedditImageEntryToDisk(imageEntry, targetDirectory);//saves to the folder
+                }
+              });
+              memeDownloaded = true;
+            }
+            //chooses random file from dir and replies to user
+            var files = fs.readdirSync(__dirname + '/images/hot/wholesomememes/')
+            chosenFile = files[Math.floor(Math.random() * files.length)]
+            message.reply("Here is an image from r/wholesomememes", { files: ['images/hot/wholesomememes/' + chosenFile] })//sends file
+            output.delete()//delets the thinking... message
+          }
+        case "featurelist":
+          output.edit("Our full feature list can be found here:")
+
+          break;
+
+        case "None"://when luis doesn't think when its any of my other intents
+          output.edit(noneResponse[Math.floor(Math.random() * noneResponse.length)])//randomly chooses one of the random none responses
+      } // end switch
       break;
 
+/*
     case "aww":
       //gets new hot images from r/aww and puts it in /images/hot/aww
       getter.getHotImagesOfSubReddit('aww').then(function (result) {
@@ -227,7 +231,8 @@ client.on("message", async message => {
         console.log(error);
       })
       break;
-    default://when command doens't match any cases
+      */
+    default: //when command doens't match any cases
       output.edit("That's not a command!")
   }
 });
